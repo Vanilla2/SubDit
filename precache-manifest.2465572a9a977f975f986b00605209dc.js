f7a9a93f50ba7131490d35016c0fb0bf88cac729
Vanilla <solmarians05@gmail.com>
2020-07-02 21:06:24 +0300

    Updates

---
+++ precache-manifest.2465572a9a977f975f986b00605209dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56ed742cc61adb0bec0b9a137ff529f",
+    "url": "/SubDit/index.html"
+  },
+  {
+    "revision": "1911e570fa007946eb97",
+    "url": "/SubDit/static/css/main.77397056.chunk.css"
+  },
+  {
+    "revision": "b562e5d9236a06e30e83",
+    "url": "/SubDit/static/js/2.41db4d0c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SubDit/static/js/2.41db4d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1911e570fa007946eb97",
+    "url": "/SubDit/static/js/main.3003185c.chunk.js"
+  },
+  {
+    "revision": "434c7216fc1c2e9dd501",
+    "url": "/SubDit/static/js/runtime-main.b636025c.js"
+  }
+]);
